77d2e5f7a9595233877b47a3b79c90e92b5f7e0b
Torsten Freyda <torsten@freyda.de>
2017-05-03 22:07:53 +0200

    Add swagger

--- router.js
+++ router.js
@@ -10,5 +10,17 @@
 module.exports = function(app) {
 	app.get('/',require('./app/welcome/controller/').indexAction);
 	app.get('/api/audiotrack',require('./app/api/controller/audiotrack.js').getAudiotrackItems);
+	
+	
+	//setup API documentation
+	const swaggerUi = require('swagger-ui-express');
+	const swaggerDocument = require('./var/etc/swagger.json');
+	
+	var showExplorer = false;
+	var options = {};
+	var customCss = '#header { display: none }';
+	var customFavicon = '/images/favicon.png'
+	
+	app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, showExplorer, options, customCss, customFavicon));
 };
 
