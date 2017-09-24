90540cd03470eafd47f7e96d93afd095678e4579
Torsten Freyda <torsten@freyda.de>
2017-09-24 21:54:49 +0200

    Added default cover image in cover API

--- app/api/controller/cover.js
+++ app/api/controller/cover.js
@@ -37,6 +37,8 @@ function getCoverGallery(req,res) {
 	var HelperUser = require('../helper/user');
 
 	var checkAuth = HelperUser.checkAuthorization(req);
+	var defaultcover = 'public/images/' + config.get('cover.default_cover');
+	var formatcover = config.get('cover.format');
 	
 	if(checkAuth != null && checkAuth != undefined) {
 		
@@ -50,25 +52,94 @@ function getCoverGallery(req,res) {
 		if(!response_content) {
 
 			ModelCover.fetchOne(req).then(function success(row) {
-	
-				if(row.data == null) {
 
-					var json_res = JSON.stringify({
-						response:"Cover data not available"
+				if(row === null) {
+					
+					fs.access(defaultcover, fs.R_OK, function (err) {
+						if(!err) {
+							fs.readFile(defaultcover, function(err, data) {
+								if(!err) {
+									
+									sharp(data)
+										.resize(config.get('cover.gallery_width_size'))
+										.embed()
+										.sharpen()
+										.toFormat(formatcover)
+										.toBuffer(function(err, outputBuffer) {
+								
+											if (err) {
+												throw err;
+											}
+										
+											if(req.query.type == 'raw') {
+											
+												res.header({
+													'Content-Type': 'image/' + formatcover,
+												});
+										
+												var response_content = {
+													'message':outputBuffer,
+													'type':'image/' + formatcover,
+													'code':200
+												}
+											} else {
+										
+												var new_img_dimensions = imgsize(outputBuffer);
+												var json_res = JSON.stringify({
+												mime: formatcover,
+												width: new_img_dimensions.width,
+												height: new_img_dimensions.height,
+												imagedata: Buffer.from(outputBuffer).toString('base64')
+											});
+										
+											var response_content = {
+													'message': json_res,
+													'type':'json',
+													'code':200
+											}
+										}
+										
+										if(res && checkAuth.token && response_content) {
+											
+											//res.send(
+											//	Buffer.from(outputBuffer).toString('base64')
+											//);
+											
+											//Output the result of the request
+											ViewCover.output(res, checkAuth.token, response_content);
+										} else {
+											console.log('missing all parameters and object to do viwer output');
+										}
+										//Output the result of the request
+										//ViewCover.output(res, checkAuth.token, response_content);
+
+//										res.send(
+//											Buffer.from(outputBuffer).toString('base64')
+//										);
+								});
+									
+								} else {
+									console.log('Cannot read default cover image for output');
+								}
+							})
+						} else {
+							console.log('No access to default cover image for output');
+						}
 					});
 
-					var response_content = {
-						'message': json_res,
-						'type':'json',
-						'code':404
-					}
+					//var json_res = JSON.stringify({
+					//	response:"Cover data not available"
+					//});
+
+					//var response_content = {
+					//	'message': json_res,
+					//	'type':'json',
+					//	'code':404
+					//}
 
 					//Output the result of the request
-					ViewCover.output(res, checkAuth.token, response_content);
+					//ViewCover.output(res, checkAuth.token, response_content);
 				} else {
-					
-					var range = req.headers.range;
-					var readStream;
 					
 //					res.header({
 //			            'Content-Type': row.mime,
@@ -78,26 +149,39 @@ function getCoverGallery(req,res) {
 						.resize(config.get('cover.gallery_width_size'))
 						.embed()
 						.sharpen()
-						.toFormat(sharp.format.webp)
+						.toFormat(config.get('cover.format'))
 						.toBuffer(function(err, outputBuffer) {
 					
-							
 							if (err) {
 								throw err;
 							}
 							
-							var new_img_dimensions = imgsize(outputBuffer);
-							var json_res = JSON.stringify({
-								mime: row.mime,
-								width: new_img_dimensions.width,
-								height: new_img_dimensions.height,
-								imagedata: Buffer.from(outputBuffer).toString('base64')
-							});
+							if(req.query.type == 'raw') {
+								
+								res.header({
+						            'Content-Type': row.mime,
+						        });
 							
-							var response_content = {
-								'message': json_res,
-								'type':'json',
-								'code':200
+								var response_content = {
+										'message':outputBuffer,
+										'type':row.mime,
+										'code':200
+								}
+							} else {
+							
+								var new_img_dimensions = imgsize(outputBuffer);
+								var json_res = JSON.stringify({
+									mime: row.mime,
+									width: new_img_dimensions.width,
+									height: new_img_dimensions.height,
+									imagedata: Buffer.from(outputBuffer).toString('base64')
+								});
+							
+								var response_content = {
+										'message': json_res,
+										'type':'json',
+										'code':200
+								}
 							}
 							
 							//Output the result of the request
@@ -114,6 +198,7 @@ function getCoverGallery(req,res) {
 				res.send(err);
 			})
 		} else {
+			console.log('Missing some valid url parameters');
 			//Output the result of the request
 			ViewCover.output(res, null, response_content);
 		}
